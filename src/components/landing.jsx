6b28e9b9b018567efbea25d3b9d94c5e6a70b507
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-05 19:26:25 +0200

    added landing styles

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import cssModule from '../styles/landing.module.scss';
 
 const Landing = () => {
   return (
-    <div>
-      <div>
+    <div className={cssModule.root}>
+      <div className={cssModule.landing}>
         <h1>Landing</h1>
-        <button onClick={() => alert('show counter')}>Show counter</button>
+        <button
+          className={cssModule.button}
+          onClick={() => alert('show counter')}>
+          Show counter
+        </button>
       </div>
-      <div>
+      <div className={cssModule.title}>
         <h1>React useEffect</h1>
       </div>
     </div>
