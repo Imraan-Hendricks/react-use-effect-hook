9726766027d4a84a1af011fc8d30dd7a964bfcc0
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-06 00:04:01 +0200

    added logs component styles

--- src/components/logs.jsx
+++ src/components/logs.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
+import cssModule from '../styles/logs.module.scss';
 import { LogsContext } from '../context/logs';
 
 const Logs = () => {
   const { logs, clearLogs } = useContext(LogsContext);
 
   return (
-    <div>
+    <div className={cssModule.root}>
       <h1>Logs:</h1>
       {logs
         .slice(0)
@@ -14,7 +15,11 @@ const Logs = () => {
           <p>{log}</p>
         ))}
       {logs.length === 0 && <p>There are no logs</p>}
-      {logs.length > 0 && <button onClick={clearLogs}>Clear logs</button>}
+      {logs.length > 0 && (
+        <button className={cssModule.button} onClick={clearLogs}>
+          Clear logs
+        </button>
+      )}
     </div>
   );
 };
