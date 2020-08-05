4590617d31eeac27920725d8f3e195905b2095e9
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-05 17:07:31 +0200

    added landing component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Landing from './components/landing';
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Landing />
     </div>
   );
 };
---
+++ src/components/landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <div>
+        <h1>Landing</h1>
+        <button onClick={() => alert('show counter')}>Show counter</button>
+      </div>
+      <div>
+        <h1>React useEffect</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
